fded7a873110161c09637a05325c5cf26f488a79
Shyyadd <juuso.e.niemi@gmail.com>
2025-09-20 00:54:07 +0300

    Update Arvauspeli.java
    Pohja valmiina

--- Arvauspeli.java
+++ Arvauspeli.java
@@ -3,5 +3,24 @@ import java.util.Scanner;
 public class Arvauspeli {
     public static void main(String[] args) {
         
+        Scanner in = new Scanner(System.in);
+
+        
+        String nimi;
+
+        do
+        {
+            
+            System.out.println("Arvaa nimeni");
+            nimi = in.nextLine();
+            
+
+        } while (!nimi.equals("Juuso")); 
+            System.out.println("Onneksi olkoon arvasit nimen!");
+                
+                
+            
+
     }
+    
 }
